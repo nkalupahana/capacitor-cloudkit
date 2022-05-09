06f9a21fc0f2700a44be4fa1a832ae322e315733
nkalupahana <avishkank@gmail.com>
2022-05-08 23:31:49 -0700

    apple on android now functional

--- android/src/main/java/cloudkit/baseline/getbaseline/app/CloudKitPlugin.java
+++ android/src/main/java/cloudkit/baseline/getbaseline/app/CloudKitPlugin.java
@@ -1,22 +1,81 @@
 package cloudkit.baseline.getbaseline.app;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 @CapacitorPlugin(name = "CloudKit")
 public class CloudKitPlugin extends Plugin {
+    ActivityResultLauncher<Intent> launcher;
+    PluginCall lastCall;
+    OkHttpClient client = new OkHttpClient();
 
-    private CloudKit implementation = new CloudKit();
+    @Override
+    public void load() {
+        super.load();
+        launcher = getActivity().registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            JSObject ret = new JSObject();
+                            ret.put("ckWebAuthToken", result.getData().getData().toString().split("ckWebAuthToken=")[1]);
+                            lastCall.resolve(ret);
+                        } else {
+                            lastCall.reject("User closed view.");
+                        }
+                    }
+                });
+    }
 
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
 
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        ret.put("value", value);
         call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void authenticate(PluginCall call) {
+        lastCall = call;
+        String url = "https://api.apple-cloudkit.com/database/1/" + call.getString("containerIdentifier") + "/" + call.getString("environment") + "/public/users/caller?ckAPIToken=" + call.getString("ckAPIToken");
+        Request request = new Request.Builder().url(url).build();
+        try (Response response = client.newCall(request).execute()) {
+            JSONObject resp = new JSONObject(response.body().string());
+            Intent intent = new Intent(getContext(), SignInWithCloudKit.class);
+            if (!resp.getString("serverErrorCode").equals("AUTHENTICATION_REQUIRED")) {
+                call.reject("API key invalid.");
+                return;
+            }
+
+            intent.setData(Uri.parse(resp.getString("redirectURL")));
+            launcher.launch(intent);
+        } catch (IOException | JSONException e) {
+            call.reject(e.getLocalizedMessage());
+        }
     }
 }
---
+++ android/src/main/java/cloudkit/baseline/getbaseline/app/SignInWithCloudKit.java
@@ -0,0 +1,42 @@
+package cloudkit.baseline.getbaseline.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class SignInWithCloudKit extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_with_cloud_kit);
+        WebView webview = findViewById(R.id.webView);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+                super.onPageStarted(view, url, favicon);
+                if (url.contains("example.com")) {
+                    Intent intent = new Intent();
+                    intent.setData(Uri.parse(url));
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+        webview.getSettings().setJavaScriptEnabled(true);
+        Log.e("URI", getIntent().getData().toString());
+        webview.loadUrl(getIntent().getData().toString());
+    }
+
+    public void cancelLogin(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+}
