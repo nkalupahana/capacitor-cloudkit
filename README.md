2121e2cd89a5b41d81c54aa1863152041b9d8c2c
nkalupahana <avishkank@gmail.com>
2022-05-09 18:36:14 -0700

    ready to go (:/)

--- README.md
+++ README.md
@@ -9,6 +9,24 @@ npm install capacitor-cloudkit
 npx cap sync
 ```
 
+## Setup
+
+Each different platform requires a different `ckAPIToken`.
+- Web: Token configured for `postMessage`.
+- iOS: Token configured to redirect to your container's URL scheme. It'll show up on the CloudKit dashboard in a dropdown when you create an API key. Mine is `cloudkit-icloud.baseline.getbaseline.app://`, so I'll be using that as an example. I set mine to `cloudkit-icloud.baseline.getbaseline.app://callback`.
+- Android: Token configured to redirect to `https://example.com` or some similar existant but blank domain.
+
+## Setup (iOS)
+
+- Add your callback URL as a URL scheme in your app, in the Info tab. (e.g. `cloudkit-icloud.baseline.getbaseline.app`)
+- Add the following to the beginning of `func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool` in `AppDelegate`:
+
+```swift
+if (url.scheme == "PUT CALLBACK URL SCHEME HERE (e.g. cloudkit-icloud.baseline.getbaseline.app)") {
+    NotificationCenter.default.post(name: NSNotification.Name("cloudkitLogin"), object: url);
+}
+```
+
 ## API
 
 <docgen-index>
--- ios/Plugin/CloudKitPlugin.swift
+++ ios/Plugin/CloudKitPlugin.swift
@@ -1,40 +1,50 @@
 import Foundation
 import Capacitor
-import CloudKit
+import SafariServices
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(CloudKitPlugin)
-public class CloudKitPlugin: CAPPlugin {        
+public class CloudKitPlugin: CAPPlugin {
+    var safariVC: SFSafariViewController?;
+    var lastCall: CAPPluginCall?;
+    
+    @objc func safariLogin(notification: Notification) {
+        let url = notification.object as! URL;
+        self.safariVC?.dismiss(animated: true)
+        lastCall?.resolve([
+            "ckWebAuthToken": url.query?.replacingOccurrences(of: "ckWebAuthToken=", with: "")
+        ])
+    }
+    
     @objc func authenticate(_ call: CAPPluginCall) {
-        let operation = CKFetchWebAuthTokenOperation(apiToken: call.getString("ckAPIToken") ?? "")
-        let opClosure = { (webToken: String?, error: Error?) in
-            if let error = error {
-                print("start")
-                print(error)
-                print(call.getString("ckAPIToken") ?? "")
-                print("end")
-                call.reject(error.localizedDescription)
-            } else {
-                let uriComponent = NSCharacterSet(charactersIn: ";,/?:@&=+$# ").inverted
-                call.resolve([
-                    "ckWebAuthToken": webToken!.addingPercentEncoding(withAllowedCharacters: uriComponent)!
-                ])
+        lastCall = call;
+        let url = "https://api.apple-cloudkit.com/database/1/\(call.getString("containerIdentifier") ?? "")/\(call.getString("environment") ?? "")/public/users/caller?ckAPIToken=\(call.getString("ckAPIToken") ?? "")"
+        print(url)
+        URLSession.shared.dataTask(with: URL(string: url)!) {(data, response, error) in
+            guard let data = data else {
+                call.reject("Invalid API token.");
+                return;
             }
-        }
-        
-        if #available(iOS 15.0, *) {
-            operation.fetchWebAuthTokenResultBlock = { res in
-                print(call.getString("ckAPIToken") ?? "")
-                print(res)
+                        
+            if let jdata = (try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)) as? [String : Any] {
+                if let redirectURL = jdata["redirectURL"] as? String {
+                    print(redirectURL);
+                    self.safariVC = SFSafariViewController(url: URL(string: redirectURL)!);
+                    
+                    NotificationCenter.default.addObserver(self, selector: #selector(CloudKitPlugin.safariLogin), name: Notification.Name("cloudkitLogin"), object: nil)
+                    
+                    DispatchQueue.main.sync {
+                        self.bridge?.presentVC(self.safariVC!, animated: true, completion: {});
+                    }
+                    
+                    return;
+                }
             }
-        } else {
-            operation.fetchWebAuthTokenCompletionBlock = opClosure
-        }
-        
-        operation.qualityOfService = .utility
-        CKContainer(identifier: call.getString("containerIdentifier") ?? "").privateCloudDatabase.add(operation)
+            
+            call.reject("Invalid API response.");
+        }.resume()
     }
 }
